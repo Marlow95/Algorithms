e0f15ca662ea87825dbfe9396788826245dd3c99
Marlow Collins <marlowcollins95@gmail.com>
2022-03-02 21:44:48 -0600

    Alogrithms Practice

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+const twoSum = function(nums, target) {
+    
+    for(let i = 0; i < nums.length; i++){
+     for(let j = i + 1; j < nums.length; j++){
+       if(nums[i] + nums[j] === target){
+         let total = [nums[i], nums[j]]
+          return total
+       } else {
+         return false
+       }
+     }
+    } 
+};
+
+
+console.log(twoSum([5,6,7,9], 11))
+// O(n2) try to make it O(n) with hash map
