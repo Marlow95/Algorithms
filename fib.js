53ef744e842bfe64b13a342aa2bef4af3808c098
Marlow Collins <marlowcollins95@gmail.com>
2022-04-02 16:55:15 -0500

    corrections

--- fib.js
+++ fib.js
@@ -1,14 +1,10 @@
 function fib(n){
-  if(n <= 2){
-    return 0
+  if(n <= 1){
+    return n
   }
+  
   return fib(n -1) + fib(n -2)
 }
 //O(2^n) time complexity
-console.log(fib(2))
-console.log(fib(3))
-console.log(fib(4))
-console.log(fib(5))
-console.log(fib(20))
 
 module.exports = fib
--- fib.test.js
+++ fib.test.js
@@ -1,5 +1,9 @@
 const fib = require("./fib")
 
 test('Check the sum of the fibonacci sequence', () => {
-    expect(fib(2)).toBe(0)
+    expect(fib(1)).toBe(1)
+})
+
+test('Check the sum of the fibonacci sequence', () => {
+    expect(fib(9)).toBe(34)
 })
